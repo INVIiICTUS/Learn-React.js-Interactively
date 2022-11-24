438015b765fd8a51a9296186085cab6f8705c202
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-11-24 18:19:46 +0000

    1.4

--- .learn/resets/01.3-building-a-layout/app.jsx
+++ .learn/resets/01.3-building-a-layout/app.jsx
@@ -12,17 +12,8 @@ const data = {
 	},
 };
 
-const content = (
-	<div className="card m-5">
-		<img className="card-img-top" src={data.image} alt="Card image cap" />
-		<div className="card-body">
-			<h5 className="card-title">{data.cardTitle}</h5>
-			<p className="card-text">{data.cardDescription}</p>
-			<a href={data.button.url} className="btn btn-primary">
-				{data.button.label}
-			</a>
-		</div>
-	</div>
+let content = (
+		<img src={data.image} />
 );
 /**
  * define the variable 'content' here and fill it with the
